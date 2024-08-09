deecb9210f8bc37f76f5f13cb943c9a693570eb0
Rohish Angawalkar <rohish.angawalkar@gmail.com>
2024-08-09 04:45:12 +0100

    result page

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -10,20 +10,19 @@ const Result = () => {
     return (
         <>
             <Navbar />
-            <div className="antialiased min-h-screen bg-white-100">
-                <div className="flex items-center justify-center min-h-screen">
-                    <div className="space-y-5 w-11/12 lg:w-1/2 bg-white shadow-lg p-5 lg:p-10 rounded-lg">
-                        <h1 className="text-center font-bold text-6xl mb-10">
-                            {/* {isVideoDeepfake ? "AI Generated" : "Original"}  */}
+            <div className="antialiased min-h-screen ">
+                <div className="flex items-center justify-center min-h-screen p-4">
+                    <div className="space-y-5 w-full max-w-md lg:max-w-2xl bg-white shadow-lg p-6 lg:p-10 rounded-lg">
+                        <h1 className="text-center font-bold text-3xl lg:text-5xl mb-6 lg:mb-10">
                             Result
                         </h1>
-                        <p className="text-center text-6xl font-bold ">
-                            The video you watched was {isVideoDeepfake ? "a AI generated " : "a original"} video.
+                        <p className="text-center text-2xl lg:text-4xl font-bold">
+                            The video you watched was {isVideoDeepfake ? "an AI generated" : "an original"} video.
                         </p>
-                        <div className="w-full text-center">
+                        <div className="w-full text-center mt-6">
                             <Button
                                 onClick={() => navigate("/")}
-                                className="rounded-full w-1/5 bg-[#5AE579] hover:bg-[#5AE579] hover:shadow-lg hover:shadow-[#5AE579] transition duration-300"
+                                className="rounded-full w-full max-w-xs bg-[#5AE579] hover:bg-[#5AE579] hover:shadow-lg hover:shadow-[#5AE579] transition duration-300"
                             >
                                 Home
                             </Button>
